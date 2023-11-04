442595eb86b652010245b32e470ed80598ba4112
Aathira Satheesh Nair <aathirasatheeshnair@Aathiras-MacBook-Pro.local>
2023-11-04 18:00:11 -0500

    connect to mongodb

---
+++ backend/modals/LoanDataModel.js
@@ -0,0 +1,30 @@
+const {
+    Schema,
+    model
+  } = require("mongoose");
+  
+  const MySchema = new Schema({
+    monthlyIncome:{
+      type:Number},
+    creditScore:{
+      type:Number
+    },
+    appraisalValue:{
+      type:Number
+    },
+    downPayment:{
+      type:Number
+    },
+    creditCardPayment:{
+      type:Number
+    },
+    carPayment:{
+      type:Number
+    },
+  },
+  {collection: 'customer_data'}
+  );
+  
+  const TaskModel = model("test", MySchema)
+  
+  module.exports = TaskModel
--- backend/routes/server.js
+++ backend/routes/server.js
@@ -4,6 +4,24 @@ const cors = require("cors");
 
 const app = express();
 const port = 3001; // or another port that you prefer
+const mongoose=require('mongoose')
+const url = `mongodb+srv://root:rootroot@eag.z6jqmoe.mongodb.net/train_data?retryWrites=true&w=majority`;
+const CustomerData = require('../modals/TaskModel');
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+    app.get("/dogs", async (req, res) => {
+      const allDogs = await CustomerData.find({});
+      return res.status(200).json(allDogs);
+    });
 
 app.use(cors()); // Use cors middleware to allow cross-origin requests
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON bodies
