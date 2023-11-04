186671ecf4b78ac52840a772527bd93097a94d41
Tejas Kalpathi <60577394+TrexKalp@users.noreply.github.com>
2023-11-04 15:57:30 -0500

    added sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import Page1 from './components/Page1'
-import './App.css'
+import { useState } from "react";
+import Page1 from "./components/Page1";
+import SidebarWithHeader from "./components/SideBar";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='form-style'>
-    <Page1/>
+    <div className="form-style">
+      <SidebarWithHeader />
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Page1.jsx
+++ frontend/src/components/Page1.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   Flex,
@@ -20,117 +20,114 @@ import {
   Text,
   useColorModeValue,
   Link,
-} from '@chakra-ui/react'
-import { useState } from 'react'
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 export default function Page1() {
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
             Are you ready to buy a home? Lets Check!
           </Heading>
-         
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
-           
-            
-                <FormControl id="monthlyincome" isRequired>
-                  <FormLabel>Gross Monthly Income</FormLabel>
-                  <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
-                </FormControl>
-          
-         
-                <FormControl id="credit" isRequired>
-                  <FormLabel>Credit Score</FormLabel>
-                  <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
-                </FormControl>
-        
-          
+            <FormControl id="monthlyincome" isRequired>
+              <FormLabel>Gross Monthly Income</FormLabel>
+              <NumberInput defaultValue={15} min={10} max={20}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <FormControl id="credit" isRequired>
+              <FormLabel>Credit Score</FormLabel>
+              <NumberInput defaultValue={15} min={10} max={20}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
             <FormControl id="Appraisal" isRequired>
               <FormLabel>Appraisal Value</FormLabel>
               <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
             </FormControl>
 
             <FormControl id="downpayment" isRequired>
               <FormLabel>Down Payment</FormLabel>
               <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
             </FormControl>
 
             <FormControl id="creditcard" isRequired>
               <FormLabel>Credit Card Payment</FormLabel>
               <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
             </FormControl>
             <FormControl id="carpayment" isRequired>
               <FormLabel>Car Payment</FormLabel>
               <NumberInput defaultValue={15} min={10} max={20}>
-  <NumberInputField />
-  <NumberInputStepper>
-    <NumberIncrementStepper />
-    <NumberDecrementStepper />
-  </NumberInputStepper>
-</NumberInput>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
             </FormControl>
 
             <Stack spacing={10} pt={2}>
               <Button
                 loadingText="Submitting"
                 size="lg"
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }}>
+                  bg: "blue.500",
+                }}
+              >
                 Check Eligibility
               </Button>
             </Stack>
-           
           </Stack>
         </Box>
       </Stack>
     </Flex>
-  )
+  );
 }
