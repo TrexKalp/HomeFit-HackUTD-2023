7311d4cd236ce8f702f549bc52efd8c6ed1980c9
Tejas Kalpathi <60577394+TrexKalp@users.noreply.github.com>
2023-11-05 07:28:45 -0600

    graphing works somewhat

--- frontend/src/components/Graphs.jsx
+++ frontend/src/components/Graphs.jsx
@@ -1,49 +1,98 @@
 import React, { useState, useEffect } from "react";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const colors = [
+  "#8884d8",
+  "#82ca9d",
+  "#ffc658",
+  "#ff8042",
+  "#0088FE",
+  "#00C49F",
+  "#FFBB28",
+  "#FF8042",
+  "#00C49F",
+]; // Add more colors as needed
 
 const Graphs = () => {
-  const [approvalData, setApprovalData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    const fetchApprovalStats = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:3001/api/approval-counts"
+        const response = await axios.get("http://localhost:3001/get-csv", {
+          responseType: "text",
+        });
+        const rows = response.data.split("\n");
+        const headers = rows[0].split(",");
+        const parsedData = rows.slice(1).map((row) =>
+          row.split(",").reduce((acc, val, index) => {
+            acc[headers[index]] = val;
+            return acc;
+          }, {})
         );
-        setApprovalData([
-          { name: "Approved", value: response.data.approved },
-          { name: "Not Approved", value: response.data.notApproved },
-        ]);
+        setData(parsedData);
       } catch (error) {
-        console.error("Error fetching approval stats:", error);
+        console.error("Error fetching CSV data:", error);
       }
     };
 
-    fetchApprovalStats();
+    fetchData();
   }, []);
 
-  const COLORS = ["#0088FE", "#FF8042"];
-
   return (
-    <PieChart width={400} height={400}>
-      <Pie
-        data={approvalData}
-        cx={200}
-        cy={200}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-      >
-        {approvalData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-      <Legend />
-    </PieChart>
+    <div style={{ width: "100%", height: 300 }}>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="CREDIT_SCORE" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="GROSS_MONTHLY_INCOME"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="CREDIT_SCORE" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="GROSS_MONTHLY_INCOME"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="EST_MONTHLY_MORTGAGE_PAYMENT"
+            stroke="#82ca9d"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
