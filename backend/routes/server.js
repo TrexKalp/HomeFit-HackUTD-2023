8fc9da211e7ae043aa5199a151afa0c21c1ad301
Utkarsh Farkya <utkarsh@sicada.local>
2023-11-04 21:14:18 -0500

    returns fields based on which loan is rejected

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -6,6 +6,8 @@ const fs = require("fs");
 const app = express();
 const upload = multer({ dest: "uploads/" });
 const mongoose = require("mongoose");
+const CustomerData = require("../modals/LoanDataModel.js");
+
 
 // Database connection parameters
 const connectionParams = {
@@ -14,7 +16,7 @@ const connectionParams = {
 };
 
 // Replace with your actual MongoDB connection string
-const url = `mongodb+srv://root:rootroot@eag.z6jqmoe.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://root:rootroot@eag.z6jqmoe.mongodb.net/train_data?retryWrites=true&w=majority`;
 
 mongoose
   .connect(url, connectionParams)
@@ -22,12 +24,12 @@ mongoose
   .catch((err) => console.error(`Error connecting to the database.\n${err}`));
 
 
-const getFieldData = (fields) => {
+const getFieldData = async (fields) => {
   const projection = {};
   for(let i = 0; i < fields.length; i++) {
     projection[fields[i]] = 1;
   }
-  const data = CustomerData.find({})
+  const data = await CustomerData.find({}, projection);
   console.log(data);
   return data;
 };
@@ -35,7 +37,7 @@ const getFieldData = (fields) => {
 app.use(cors());
 app.use(express.json());
 // Function to check eligibility, extracted from your existing logic
-function checkEligibility(data) {
+async function checkEligibility(data) {
   const income = parseFloat(data.GrossMonthlyIncome);
   const carPayment = parseFloat(data.CarPayment);
   const creditCardPayment = parseFloat(data.CreditCardPayment);
@@ -47,7 +49,7 @@ function checkEligibility(data) {
   const score = parseInt(data.CreditScore, 10);
 
   // Your existing calculations (adjusted if necessary for consistency with your requirements)
-  const LTV = (loanAmount / appraisedValue) * 100;
+  const LTV = (appraisedValue - downPayment / appraisedValue) * 100;
   const monthlyDebtPayments =
     carPayment + creditCardPayment + studentPayment + mortgagePayment;
   const DTI = (monthlyDebtPayments / income) * 100;
@@ -77,12 +79,6 @@ function checkEligibility(data) {
     fields.push("FEDTI")
   }
 
-  let filteredData = null;
-  if(approved === false) {
-    filteredData = getFieldData(fields);
-    console.log(filteredData)
-  }
-
   return {
     approved,
     PMI,
@@ -90,17 +86,7 @@ function checkEligibility(data) {
   };
 }
 
-// // Function to format the response consistently for both endpoints
-// function formatResponse({ approved, PMI, suggestions }) {
-//   return {
-//     approved: approved ? "Yes" : "No",
-//     PMI:
-//       PMI !== null ? `Required - $${PMI.toFixed(2)} per month` : "Not Required",
-//     suggestions,
-//   };
-// }
-
-app.post("/api/process-batch", upload.single("file"), (req, res) => {
+app.post("/api/process-batch", upload.single("file"), async (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file uploaded.");
   }
@@ -190,15 +176,20 @@ app.post("/api/check-eligibility", async (req, res) => {
   let approved = score >= 640 && LTV < 95 && DTI < 43 && FEDTI <= 28;
   let PMI = null;
   let suggestions = [];
+  let fields = [];
 
   if (LTV >= 80 && LTV < 95) {
     PMI = (loanAmount * 0.01) / 12; // PMI calculation
   }
 
   if (!approved) {
-    if (score < 640) suggestions.push("Improve your credit score.");
+    if (score < 640) {
+      suggestions.push("Improve your credit score.");
+      fields.push("score");
+    }
     if (LTV >= 80) {
       suggestions.push("Increase your down payment amount.");
+      fields.push("LTV");
       if (PMI) {
         suggestions.push(
           `Consider the additional cost of Private Mortgage Insurance (PMI): $${PMI.toFixed(
@@ -208,14 +199,17 @@ app.post("/api/check-eligibility", async (req, res) => {
       }
     }
     if (DTI >= 43) suggestions.push("Pay off some current debt.");
+    fields.push("DTI");
     if (FEDTI > 28) suggestions.push("Look for a less expensive home.");
+    fields.push("FEDTI");
   }
-
+  let filteredData = null;
   // Now, increment counters based on the approval status
   if (approved) {
     approvalStatistics.approvedCount++;
   } else {
     approvalStatistics.notApprovedCount++;
+    filteredData = await getFieldData(fields);
   }
 
   // Prepare data for the CSV file
@@ -242,6 +236,7 @@ app.post("/api/check-eligibility", async (req, res) => {
       PMI: PMI ? `Required - $${PMI.toFixed(2)} per month` : "Not Required",
       suggestions: suggestions,
       statistics: approvalStatistics,
+      fields: filteredData
     });
   } catch (error) {
     // If an error occurs, send a 500 server error response
